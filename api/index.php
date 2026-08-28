7ba9b418ed673d5b0aaf37ac91d8441b8339f93b
TomCDX <tomipmgr@gmail.com>
2026-08-28 10:19:09 +0700

    fix php routing for vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,28 @@
 <?php
 
-require_once __DIR__ . '/../index.php';
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$request = urldecode($request);
+
+if ($request === '/' || $request === '') {
+    require_once __DIR__ . '/../index.php';
+    exit;
+}
+
+$file = realpath(__DIR__ . '/..' . $request);
+
+$root = realpath(__DIR__ . '/..');
+
+if (
+    $file &&
+    $root &&
+    str_starts_with($file, $root) &&
+    is_file($file) &&
+    pathinfo($file, PATHINFO_EXTENSION) === 'php'
+) {
+    require_once $file;
+    exit;
+}
+
+http_response_code(404);
+echo '404 - Page Not Found';
